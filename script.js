108c488aa12fe7add42585c986d3eb6f395d35a1
lev <aleoncom01@gmail.com>
2025-09-21 13:21:50 +0300

    almost finish index

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+ 
+const faqButton = document.querySelectorAll('.faq-card-question-right')
+faqButton.forEach(function(element,index){
+  element.addEventListener('click',function(){
+          element.parentElement.nextElementSibling.nextElementSibling.classList.toggle('show-answer')
+          element.classList.toggle('cross')
+    })
+})
