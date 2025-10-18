6821fc4329879d2cee6ad4e358d46351ee9750c3
lev <aleoncom01@gmail.com>
2025-10-18 11:46:24 +0200

    js start

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ faqButton.forEach(function(element,index){
           element.classList.toggle('cross')
     })
 })
+
+const overlay = document.querySelector('.overlay')
+const cross = document.querySelector('.overlay-modal-cross')
+cross.addEventListener('click',function(){
+  overlay.style.display='none'
+})
+
+const button = document.querySelectorAll('.editions-cards-div-button')
+const price = document.querySelector('.overlay-modal-form-price-item')
+button.forEach(element => {
+  element.addEventListener('click',function(){
+    overlay.style.display='flex'
+    price.textContent=element.previousElementSibling.textContent
+  })
+});
+
+    const video = document.querySelector('video');
+
+ document.getElementById('showVideo').addEventListener('click', () => {
+    video.src = 'Бог Війни - Рагнарок ТРЕЙЛЕР УКРАЇНСЬКОЮ (God of War Ragnarok) @HUMANWASDGames.mp4';
+    video.controls = true;
+
+    video.style.display = 'flex'
+    document.getElementById('videoContainer').appendChild(video);
+  });
